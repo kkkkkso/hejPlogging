5510dc51ae95a98dd11af31a7ae8e2a2ae0ad063
kkkkkso <qaws4286@gmail.com>
2022-05-26 02:21:28 +0900

    테이블별로 VO폴더/파일생성

---
+++ src/com/campagin/domain/vo/CampaginVO.java
@@ -0,0 +1,18 @@
+package com.campagin.domain.vo;
+
+
+public class CampaginVO {
+	
+	private String userId;
+	private String userPw;
+	private String userName; 
+	private String userPhonenumber; 
+	private String userGender; 
+	private String userBirth; 
+	private String userImg; 
+	int userPoint; 
+	private String userEmail; 
+	
+	
+	
+}
