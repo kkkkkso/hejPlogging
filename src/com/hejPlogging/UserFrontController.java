4f1a7a657129ffc7ee213cf7bafc22e246f1959f
polodumbo <polosh1208@gmail.com>
2022-05-31 19:54:06 +0900

    UserJoinOk.java, UserLoginOk.java 작성 UserDAO, Mapper 수정

--- src/com/hejPlogging/UserFrontController.java
+++ src/com/hejPlogging/UserFrontController.java
@@ -10,7 +10,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.hejPlogging.UserJoinOk;
 import com.hejPlogging.action.ActionInfo;
 
 public class UserFrontController extends HttpServlet {
@@ -30,16 +29,17 @@ public class UserFrontController extends HttpServlet {
 		String command = requestURL.substring(requestURL.lastIndexOf("/") + 1);
 		ActionInfo actionInfo = null;
 		
-		if(command.equals(".us")) {
-			actionInfo = new UserJoinOk().execute(req, resp);
-		} else if(command.equals("")) {
+		if (command.equals("UserJoinOk.us")) {
+			actionInfo = new UserLoginOk().execute(req, resp);
+		} else if (command.equals("Join.us")) {
 			actionInfo = new ActionInfo();
-			actionInfo.setRedirect(true); 
-			actionInfo.setPath(req.getContextPath() + "/join.jsp");
-		}  else {
+			actionInfo.setRedirect(true);
+			actionInfo.setPath(req.getContextPath() + "/header.jsp");
+		} else if (command.equals("UserLoginOk.us")) {
+			actionInfo = new UserLoginOk().execute(req, resp);
+		} else {
 			// 404 일 때 출력할 에러 페이지 경로 작성
 		}
-		
 		
 		if(actionInfo != null) {
 			if(actionInfo.isRedirect()) {
@@ -51,5 +51,5 @@ public class UserFrontController extends HttpServlet {
 		}
 		
 	}
-	
 }
+	
--- src/com/hejPlogging/UserJoinOk.java
+++ src/com/hejPlogging/UserJoinOk.java
@@ -1,8 +1,7 @@
 package com.hejPlogging;
 
 import java.io.IOException;
-
-
+import java.util.Base64;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -22,12 +21,13 @@ public class UserJoinOk implements Action {
 		UserDAO userDAO = new UserDAO();
 		
 		userVO.setUserId(req.getParameter("userId"));
-		userVO.setUserPw(req.getParameter("userPw"));
+		userVO.setUserPw(new String(Base64.getEncoder().encode(req.getParameter("userPw").getBytes())));
 		userVO.setUserName(req.getParameter("userName"));
 		userVO.setUserPhonenumber(req.getParameter("userPhonenumber"));
 		userVO.setUserGender(req.getParameter("userGender"));
 		userVO.setUserBirth(req.getParameter("userBirth"));
 		userVO.setUserImg(req.getParameter("userImg"));
+		userVO.setUserPoint(Integer.parseInt(req.getParameter("userPoint")));
 		userVO.setUserEmail(req.getParameter("userEmail"));
 
 		userDAO.join(userVO);
@@ -35,7 +35,7 @@ public class UserJoinOk implements Action {
 		req.setAttribute("UserName", userVO.getUserName());
 		
 		actionInfo.setRedirect(false);
-//		actionInfo.setPath("/joinSuccess.jsp");
+		actionInfo.setPath("/mainPage.jsp");
 		
 		return actionInfo;
 	}
